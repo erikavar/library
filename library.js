2e9c8e3c2c3859c2ba1738b953812e37ee8dbb63
Erika <erikavarghese@gmail.com>
2022-08-05 23:24:20 -0400

    Added comments throughout

--- library.js
+++ library.js
@@ -1,5 +1,7 @@
+// Array to store the books
 let myLibrary = [];
 
+// Book object
 function Book(title, author, pages, readOrNot) {
     this.title = title;
     this.author = author;
@@ -7,19 +9,21 @@ function Book(title, author, pages, readOrNot) {
     this.readOrNot = readOrNot;
 }
 
+// Function to change "read" status for any book
 Book.prototype.changeReadStatus = function() {
-  if (this.readOrNot === "read") {
-    this.readOrNot = "not read yet";
-  } else {
-    this.readOrNot = "read";
-  }
+  this.readOrNot === "read" ? this.readOrNot = "not read yet" : this.readOrNot = "read";
   displayBooks();
 }
 
-function NewBook() { }
 
+// Using Object.create to return a new object with the Book prototype
+function NewBook() { }
 NewBook.prototype = Object.create(Book.prototype);
 
+// Adding books to library using form input and button
+// Creating new book object with properties pulled from user input values
+// preventDefault() to stop form from submitting
+// Resetting form
 function addBookToLibrary(ev) {
   ev.preventDefault();
   let newBook = new NewBook();
@@ -31,16 +35,26 @@ function addBookToLibrary(ev) {
   document.querySelector("form").reset();
 }
 
+document.querySelector(".add").addEventListener("click", addBookToLibrary);
+
+// Want to be able to select container so that we can clear "empty library" text
 let box = document.getElementById("container");
 
-
+// Displaying books in the myLibrary array
 function displayBooks () {
   box.textContent = "";
+
+  // Iterating through the array of book objects (items)
   myLibrary.forEach(function (item, index) { 
+    
+    // Making a div for each book object, setting class as "book" for styling, adding it to the container
     const div = document.createElement("div");
     div.setAttribute("class", "book");
     box.appendChild(div);
 
+    // Making divs for titles and authors
+    // Setting classes based on string length for titles and authors so that we can shrink text if needed
+    // Setting content displayed in bookTitle and bookAuthor divs to be equal to the title and author of each book object (item)
     const bookTitle = document.createElement("div");
     if (item.title.length > 20) {
       bookTitle.setAttribute("class", "bookTitleShrink");
@@ -59,11 +73,13 @@ function displayBooks () {
     bookAuthor.textContent = item.author;
     div.appendChild(bookAuthor);
 
+    // Div to display page number
     const pageNumber = document.createElement("div");
     pageNumber.setAttribute("class", "pageNumber");
     pageNumber.textContent = item.pages + " pages";
     div.appendChild(pageNumber);
 
+    // Button that will have different class and text depending on whether book object (item) is read or no
     const statusToggle = document.createElement("button");
     if (item.readOrNot === "read") {
       statusToggle.textContent = "Read ✓";
@@ -73,13 +89,17 @@ function displayBooks () {
       statusToggle.setAttribute("class", "toggleNotRead");
     }
     div.appendChild(statusToggle);
+    
+    // Need to use bind here otherwise would be global "this":
     statusToggle.addEventListener("click", item.changeReadStatus.bind(item));
 
+    // Adding button to remove book
     const removeBtn = document.createElement("button");
     removeBtn.setAttribute("class", "remove");
     removeBtn.textContent = "Remove";
     div.appendChild(removeBtn);
 
+    // Remove book object both visually and from array when "remove" is clicked
     div.setAttribute("data-index-number", index);
     removeBtn.addEventListener("click", function() {
       box.removeChild(div);
@@ -88,18 +108,19 @@ function displayBooks () {
   });
 }
 
+document.querySelector(".btn").addEventListener("click", displayBooks);
+
+//Open form when we click the "Add a book" button
 function openForm() {
   document.getElementById("myForm").style.display = "block";
 }
 
 document.querySelector(".button").addEventListener("click", openForm);
 
+// Close form when we click the "cancel" button
 function closeForm() {
   document.getElementById("myForm").style.display = "none";
 }
 
 document.querySelector(".cancel").addEventListener("click", closeForm);
 
-document.querySelector(".add").addEventListener("click", addBookToLibrary);
-
-document.querySelector(".btn").addEventListener("click", displayBooks);
